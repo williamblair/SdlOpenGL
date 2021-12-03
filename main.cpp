8d53f9d54b2650bc13e748838dc7378cc6e9a538
williamblair <bblair12876@aol.com>
2021-12-02 22:35:09 -0500

    started adding gltf

--- main.cpp
+++ main.cpp
@@ -9,10 +9,59 @@
 #include "Renderer.h"
 #include "Shader.h"
 #include "Mesh.h"
+#include "cgltf.h"
 
 #ifdef WIN32
 #undef main
 #endif
+
+class GLTFMesh
+{
+public:
+    GLTFMesh() :
+        mData( nullptr )
+    {
+    }
+
+    ~GLTFMesh()
+    {
+        if ( mData ) {
+            cgltf_free( mData );
+            mData = nullptr;
+        }
+    }
+
+    bool Load(const char* filename)
+    {
+        cgltf_options options;
+        memset(&options, 0, sizeof(options));
+
+        cgltf_result result = cgltf_parse_file(&options, filename, &mData);
+        if (result != cgltf_result_success) {
+            printf("Failed to load gltf %s\n", filename);
+            return false;
+        }
+
+        result = cgltf_load_buffers(&options, mData, filename);
+        if (result != cgltf_result_success) {
+            cgltf_free(mData);
+            printf("Failed to load buffers for gltf %s\n", filename);
+            return false;
+        }
+
+        result = cgltf_validate(mData);
+        if (result != cgltf_result_success) {
+            cgltf_free(mData);
+            printf("Failed to validate file %s\n", filename);
+            return false;
+        }
+
+        return true;
+    }
+
+private:
+    cgltf_data* mData;
+};
 
 int main()
 {
@@ -38,6 +87,11 @@ int main()
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     mesh.Rotate( glm::radians(45.0f), glm::vec3(0.0f,0.0f,1.0f) );
+
+    GLTFMesh gltf;
+    if ( !gltf.Load("data/Woman.gltf") ) {
+        return 1;
+    }
 
     // render loop
     // -----------
